edb8dd4478379d51ea8afb770abaa5af6a43fb1c
arifpehlivan <arifpehlivanapap@gmail.com>
2022-04-08 04:39:26 +0300

    basic files

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+import React, { useState, useEffect, Component } from 'react';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+
+root.render(<App/>);
